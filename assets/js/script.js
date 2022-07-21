3b6e206cb4e37ddf0fca77f7f29ecdf1e94a9c24
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-07-21 13:42:25 -0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -28,21 +29,25 @@ var taskFormHandler = function(event) {
     var createTaskEl = function(taskDataObj) {
        // create list item
         var listItemEl = document.createElement("li"); 
-         listItemEl.className = "task-item";
-    
-        // create div to hold task info and add to list item
-        var taskInfoEl = document.createElement("div");
+        listItemEl.className = "task-item";
 
+        // add task id as a customer attribute
+        listItemEl.setAttribute("data-task-id", taskIdCounter);
+    
         // give it a class name
+        var taskInfoEl = document.createElement("div");
         taskInfoEl.className = "task-info";
 
         // add HTML content to div
         taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
         listItemEl.appendChild(taskInfoEl);
 
         // add entire list item to list
         tasksToDoEl.appendChild(listItemEl);
+
+        // increase task counter for next unique id
+        taskIdCounter++;
+
     };
 
   
